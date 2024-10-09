1c65bc6ed5b5b2a502920556d1bba33eda068e9b
younesselrag <youness.elbrag@aigot,com>
2024-10-09 03:56:40 +0100

    build the method Dooubly Linked List

---
+++ Udmey/BigO/bigO.py
@@ -0,0 +1,32 @@
+
+def BigO(number_operation):
+    for i in range(number_operation):
+        for j in range(number_operation):
+            print(i , j)
+
+    for k in range(number_operation):
+        print(k)
+
+def findElement(Element):
+    division = 2
+    array = [1,2,3,4,5,6,7,8,]
+    for 
+    for val in array:
+
+
+ 
+
+if __name__ == "__main__":
+    """
+    now in this case we can Drop Mon-Domainat O(n)
+    which in this function Operations takes as follow:
+    
+    - instead Loop of (i ,j) --> O(n^2)
+    - k loop is --> O(n)
+
+    the Big Complexcity of this function is : BigO(N)== (N^2 + N)
+
+    but the domnainat operation is N^2 which N isn't impact on operation which could make it as folllow : --> bigO is O(N^2)
+
+    """
+    case = BigO(10)
--- src/Data_Structure/DoublyLL/DoublyLL.py
+++ src/Data_Structure/DoublyLL/DoublyLL.py
@@ -19,11 +19,50 @@ class DoublyLinklist:
             self.head = new_node
             self.tail = new_node
         
-        self.tail.next = new_nodels 
+        self.tail.next = new_node 
         new_node.pres = self.tail
         self.tail = new_node
         self.length +=1
         return True
+
+    def pop(self):
+
+        if self.length == 0 :
+            return False
+
+        if self.length == 1:
+            self.tail = None 
+            self.head = None 
+        else:
+            temp = self.tail
+            self.tail = self.tail.pres 
+            self.tail.next = None 
+            temp.pres = None
+            self.length -= 1
+        return True
+
+    def repend(self,value):
+        new_node = Node(value=value)
+
+        if self.head is None :
+            self.head = new_node
+            self.tail = new_node
+        
+        else:
+            temp = self.head
+            new_node.next = temp
+            self.head = new_node
+            self.length += 1
+
+        return True
+
+
+
+        
+        
+
+
+
 
         
 
@@ -47,6 +86,10 @@ if __name__ == "__main__":
     DLL.append(5)
     DLL.append(4)
     DLL.append(4)
+    #DLL.pop()
+    DLL.repend(4)
+    DLL.repend(8)
+
 
 
     DLL.print_dll()
