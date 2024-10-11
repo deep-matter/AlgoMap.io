e5d6c9c6655da5310a49c2a0a38bba1c5de1405f
younesselrag <youness.elbrag@aigot,com>
2024-10-11 02:30:00 +0100

    done all Doubly linklsit

--- src/Data_Structure/DoublyLL/DoublyLL.py
+++ src/Data_Structure/DoublyLL/DoublyLL.py
@@ -18,7 +18,7 @@ class DoublyLinklist:
         if self.head is None:
             self.head = new_node
             self.tail = new_node
-        
+
         self.tail.next = new_node 
         new_node.pres = self.tail
         self.tail = new_node
@@ -87,22 +87,62 @@ class DoublyLinklist:
 
         return forward
 
-            
+    def set_value(self,index , value):
+        if index < 0 or index > self.length:
+            return False
+        else:
+            get_node = self.get_index_node(index)
+            get_node.value = value
+        return True
 
+    def insert_node(self,index,value):
+        new_node = Node(value=value)
 
+        if index < 0 or index > self.length:
+            return False
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        
+        if index == 0 :
+            self.repend(value=value)
+        
+        if index == self.length:
+            self.append(value=value)
 
         
+        before = self.get_index_node(index=index - 1)
+        after = before.next
+        new_node.pres = before
+        new_node.next = after
+        before.next = new_node
+        after.pres = new_node
+        self.length += 1
+        
+        return True 
         
 
+    def remove_node_at_index(self, index):
+        if index < 0 or index > self.length:
+            return False 
 
+        if index == 0 :
+            self.pop_first()
+        if index == self.length:
+            self.pop()
 
+        before = self.get_index_node(index - 1)
+        temp = before.next
+        after_next = temp.next
+
+        before.next = after_next
+        after_next.pres = before 
+        temp = None 
+
+        self.length -= 1
 
         
-
-
-
-
-
 
     def print_dll(self):
         temp = self.head 
@@ -123,10 +163,13 @@ if __name__ == "__main__":
     DLL.repend(4)
     #DLL.repend(8)
     DLL.pop_first()
-    DLL.get_index_node(0)
-    print(DLL.get_index_node(0).value)
-    print(DLL.get_index_node(2).value)
 
-
+    #DLL.get_index_node(0)
+    # print(DLL.get_index_node(0).value)
+    # print(DLL.get_index_node(2).value)
+    DLL.set_value(1,3)
+    #DLL.insert_node(3,6)
+    DLL.remove_node_at_index(2)
+    DLL.remove_node_at_index(1)
     DLL.print_dll()
 
