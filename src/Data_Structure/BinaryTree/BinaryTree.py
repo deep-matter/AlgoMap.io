8cc1af3b4691baabd6e5219533c2a1a78b1f2480
younesselrag <youness.elbrag@aigot,com>
2024-10-13 05:50:17 +0100

    tree Bimary

--- src/Data_Structure/BinaryTree/BinaryTree.py
+++ src/Data_Structure/BinaryTree/BinaryTree.py
@@ -8,17 +8,63 @@ class Node(object):
 
 
 class BinaryTree(object):
-    def __init__(self,value):
-        node = Node(value=value)
-        self.root = node
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value=value)
+
+        if self.root == None:
+            self.root = new_node
+            return True
+
+        else:
+            temp = self.root
+            while (True):
+                if temp.value == new_node.value:
+                    return False 
+                if temp.value > new_node.value:
+                    if temp.left is None:
+                        temp.left = new_node
+                        return True
+                    temp = temp.left
+                else:
+                    if temp.right is None:
+                        temp.right = new_node
+                        return True
+
+                    temp = temp.right
+        
+
+
+        
+
+        
+        
 
     def print_tree(self):
+        if self.root == None :
+            print(self.root)
         tree = self.root
-        while tree.left is not None and tree.right is not None:
+        while tree is not None:
             print(tree.value)
+            if tree.left:
+                tree
+            else:
+                tree = tree.right
 
 
 
 if __name__ =="__main__":
-    tree = BinaryTree(1)
+    tree = BinaryTree()
+    tree.insert(1)
+    tree.insert(2)
+    tree.insert(3)
+    tree.insert(4)
+    tree.insert(2)
+
+
+
+
+
     tree.print_tree()
